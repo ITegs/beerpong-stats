f7268a20274afad05a6e2627622c4d3b94f51530
Johannes <johannes-pahle@t-online.de>
2021-06-20 10:31:15 +0200

    tournament addGame;

--- src/components/tournament.tsx
+++ src/components/tournament.tsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import {
   IonButton,
   IonContent,
+  IonHeader,
   IonIcon,
   IonItem,
   IonLabel,
   IonList,
+  IonModal,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
-import { add, code } from "ionicons/icons";
+import { add, arrowForwardCircle, code } from "ionicons/icons";
 
 import "./tournament.css";
 import { playerData, gameStats } from "../data/data";
@@ -15,6 +21,9 @@ import { playerData, gameStats } from "../data/data";
 const Tournament: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
   const [showAddPlayerAlert, setShowAddPlayerAlert] = useState(false);
+
+  const [curRound, setCurRound] = useState<number>(0);
+  const [winner, setWinner] = useState<any>("");
 
   const [state, setState] = useState(0);
   const [teams, setTeams] = useState([
@@ -27,6 +36,8 @@ const Tournament: React.FC = () => {
     [0, 1],
     [2, 3],
   ]);
+
+  function addGame() {}
 
   return (
     <>
@@ -55,20 +66,74 @@ const Tournament: React.FC = () => {
           {rounds.map((r: any, i: number) => (
             <IonItem key={i}>
               <IonLabel slot="start">
-                {teams[r[0]].map(
-                  (t: any, ii: number) => playerData[t].name + " "
-                )}
+                {teams[r[0]].map((t: any, ii: number) => (
+                  <div key={ii}>{playerData[t].name}</div>
+                ))}
               </IonLabel>
-              <IonIcon icon={code} />
-              <IonLabel slot="end">
-                {teams[r[1]].map(
-                  (t: any, ii: number) => playerData[t].name + " "
-                )}
+              <IonLabel slot="start" color="danger">
+                VS.
               </IonLabel>
+              <IonLabel slot="start">
+                {teams[r[1]].map((t: any, ii: number) => (
+                  <div key={ii}>{playerData[t].name}</div>
+                ))}
+              </IonLabel>
+              <IonButton
+                fill="outline"
+                slot="end"
+                onClick={() => (setCurRound(r), setShowModal(true))}
+              >
+                <IonIcon icon={arrowForwardCircle} />
+              </IonButton>
             </IonItem>
           ))}
         </IonList>
       ) : null}
+      <IonModal isOpen={showModal}>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Spiel eintragen</IonTitle>
+            <IonButton
+              slot="end"
+              fill="outline"
+              size="small"
+              onClick={() => setShowModal(false)}
+            >
+              Abbrechen
+            </IonButton>
+          </IonToolbar>
+        </IonHeader>
+        <div className="selectContainer">
+          <IonLabel>Teams:</IonLabel>
+          <div className="selector">
+            {rounds[curRound].map((r: any, i: number) => {
+              teams[r].map((t: any, ii: number) => (
+                <div key={ii}>{playerData[t].name + " "}</div>
+              ));
+            })}
+          </div>
+          <div className="selector">lol</div>
+        </div>
+
+        <div className="winnerContainer">
+          <IonLabel>Gewinner Team:</IonLabel>
+          <div className="winnerSegment">
+            <IonSegment onIonChange={(e) => setWinner(e.detail.value)}>
+              <IonSegmentButton value="red">
+                <strong>Rot</strong>
+              </IonSegmentButton>
+              <IonSegmentButton value="blue">
+                <strong>Blau</strong>
+              </IonSegmentButton>
+            </IonSegment>
+          </div>
+        </div>
+        <div className="addGameButton">
+          <IonButton fill="outline" size="large" onClick={() => addGame()}>
+            <IonIcon icon={add} />
+          </IonButton>
+        </div>
+      </IonModal>
     </>
   );
 };
