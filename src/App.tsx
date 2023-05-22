619a173c693df0632d8331593307db52003fa201
Johannes Pahle <joe@jo-dev.de>
2023-05-22 23:27:04 +0200

    ✨ add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="HeaderDouble">
+      <div className="HeaderContainer">
+        <img src="/icon.png" alt="Beerpong Logo" />
+        <h1>Beerpong Stats</h1>
+      </div>
+    </div>
+  );
+}
