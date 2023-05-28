4f092197c0946aee38e3196ded92b476c5a722f0
Johannes Pahle <joe@jo-dev.de>
2023-05-28 19:17:53 +0200

    💄 `NavBar` styling fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,20 +4,28 @@ import "./NavBar.css";
 export default function NavBar(props: { active: string; setActive: Function }) {
   return (
     <div className="NavBarContainer">
-      <img
-        src="/home.svg"
-        alt="Home"
-        className={props.active === "home" ? "NavBarActive" : "NavBarInActive"}
-        onClick={() => props.setActive("home")}
-      />
-      <img
-        src="/tournament.svg"
-        alt="Tournament"
-        className={
-          props.active === "tournament" ? "NavBarActive" : "NavBarInActive"
-        }
-        onClick={() => props.setActive("tournament")}
-      />
+      <div className="home">
+        <img
+          src="/home.svg"
+          alt="Home"
+          className={
+            props.active === "home" ? "NavBarActive" : "NavBarInActive"
+          }
+          onClick={() => props.setActive("home")}
+        />
+        <p>Home</p>
+      </div>
+      <div className="tournament">
+        <img
+          src="/tournament.svg"
+          alt="Tournament"
+          className={
+            props.active === "tournament" ? "NavBarActive" : "NavBarInActive"
+          }
+          onClick={() => props.setActive("tournament")}
+        />
+        <p>Tournament</p>
+      </div>
     </div>
   );
 }
