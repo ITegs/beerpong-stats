d0845839b75465f0f38281dd3f6776f0e08ac60d
Johannes Pahle <joe@jo-dev.de>
2023-05-28 19:18:36 +0200

    ♻️ `Header` + `NavBar` fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 export default function App() {
+  const [active, setActive] = React.useState("home");
+
   return (
     <div className="App">
-      <Header />
+      <div className="Header">
+        <Header />
+      </div>
+      <div className="sep" />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      <div className="sep" />
+      <div className="NavBar">
+        <NavBar active={active} setActive={setActive} />
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,19 +4,13 @@ import "./Home.css";
 import Ranking from "../components/Ranking";
 import PlayerCard from "../components/PlayerCard";
 import GamesCard from "../components/GamesCard";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
-  const [active, setActive] = React.useState("home");
-
   return (
     <div>
       <Ranking />
       <PlayerCard />
       <GamesCard />
-      <div className="HomeNavBar">
-        <NavBar active={active} setActive={setActive} />
-      </div>
     </div>
   );
 }
